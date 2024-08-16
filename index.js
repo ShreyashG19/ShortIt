b75649c30eb33c5dbe88fa90d7243f0b1cf02ed6
Shreyash Galgale <shreyasgalgale019@gmail.com>
2024-08-17 00:35:37 +0530

    ejs complete

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const path = require("path");
 const urlRoute = require("./routes/url");
+const staticRoute = require("./routes/staticRouter");
 const { connectToMongoDb } = require("./connect");
 const URL = require("./models/url");
 const app = express();
@@ -13,10 +15,23 @@ connectToMongoDb("mongodb://localhost:27017/short-it")
         console.log("MongoDB service :: connectToMongoDb :: error : ", error);
     });
 
-app.use(express.urlencoded({ extended: false }));
-app.use("/url", urlRoute);
+//setting view engine
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views")); //setting path of html files
 
-app.get("/:shortId", async (req, res) => {
+app.use(express.urlencoded({ extended: false }));
+app.get("/test", async (req, res) => {
+    const allUrls = await URL.find({});
+    return res.render("home", {
+        urls: allUrls,
+        name: "shreyash",
+    });
+});
+
+//setting routes
+app.use("/url", urlRoute);
+app.use("/", staticRoute);
+app.get("/urls/:shortId", async (req, res) => {
     const shortId = req.params.shortId;
     const entry = await URL.findOneAndUpdate(
         { shortId },
@@ -28,7 +43,7 @@ app.get("/:shortId", async (req, res) => {
             },
         }
     );
-    res.redirect(entry.redirectURL);
+    return res.redirect(entry.redirectURL);
 });
 
 app.listen(PORT, () => {
